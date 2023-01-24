cf912eab3345290738a2e5a60c65cdb35979234d
fanis18155 <fanispapa31@gmail.com>
2023-01-24 14:22:25 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const navList = document.querySelector(".nav-list");
+const navigation = document.querySelector(".navigation");
+const bars = document.querySelector(".burger-btn__bars");
+const cross = document.querySelector(".burger-btn__cross");
+
+const toggleNavBtn = () => {
+  bars.classList.toggle("hide");
+  cross.classList.toggle("hide");
+};
+
+navigation.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-solid")) {
+    navList.classList.toggle("show");
+    toggleNavBtn();
+  }
+});
